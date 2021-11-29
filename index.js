a90ee9443602bb7a4a6d5e3761c05c833b4efe2f
IlyaIvanchikov <phantomonster1@mail.ru>
2021-11-30 01:21:43 +0300

    feat: added depends_on config

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import {
+    MONGO_USER,
+    MONGO_PASSWORD,
+    MONGO_IP,
+    MONGO_PORT,
+} from './config/config.js';
 const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/mydb?authSource=admin`;
+
+const connectWithRetry = () => {
+    mongoose
+        .connect(mongoURL)
+        .then(() => console.log('connected mongoose db'))
+        .catch((e) => {
+            console.log(e);
+            setTimeout(connectWithRetry, 1000);
+        });
+};
+
+connectWithRetry();
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send(`hello111211`);
+    res.send(`hello`);
 });
 
 app.listen(PORT, () => {
